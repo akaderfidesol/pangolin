42a55e0402ff6f86f7f5579c7573741510f24c52
Angel <angelhodar76@gmail.com>
2020-04-25 18:59:47 +0200

    Added maldita and newtral body parsing

--- background_script.js
+++ background_script.js
@@ -1,63 +1,77 @@
-list_pages = ['maldita.es', 'newtral.es']
+list_pages = ["newtral.es", "maldita.es"];
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse){
-		var msg = JSON.parse(request.msg);
-		if(msg.funct == 'call'){
-			call(msg.params)
-		}
-    }
-);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.msg.title) {
+    call(request.msg.title);
+  }
+});
 
+function getHTML(url, callback) {
+  var request = new XMLHttpRequest();
 
-function call(search){
-	
-	for(var lp=0; lp<list_pages.length; lp++) {
-		var page = list_pages[lp];
-		
-		var search = 'algo'
-		
-		var xhr = new XMLHttpRequest();
-		query = search.split(' ').join('+');
-		xhr.open("GET", "https://www.google.es/search?as_q="+query+"&as_sitesearch="+page, true);
-		
-		xhr.onload = function (e) {
-		  if (xhr.readyState === 4) {
-			if (xhr.status === 200) {
-				
-				var response = xhr.responseText;
-				var res = document.createElement( 'div' );
-				res.innerHTML = response
-				data = res.querySelector("#search")
-	
-				var aNodes = data.querySelectorAll("a");
-				
-				var links = []
-				for(var i=0; i<aNodes.length; i++) {
-					for (var j=0; j<list_pages.length; j++){
-						if(aNodes[i].href.startsWith('http') && aNodes[i].href.includes(list_pages[j]) && !aNodes[i].href.includes("webcache") ){
-							console.log("SI ->"+aNodes[i].href)
-							links.push(aNodes[i].href);
-						}
-					}
-				}
-				
-				//**************************//
-				//    AQUI VAN LOS LINKS    //
-				//**************************//
-				console.log(links)
-				
-			} else {
-			  console.error(xhr.statusText);
-			}
-		  }
-		};
-		
-		xhr.onerror = function (e) {
-		  console.error(xhr.statusText);
-		};
-		
-		xhr.send(null); 
-	}
-	
+  request.onload = function () {
+    if (this.readyState == 4 && this.status == 200)
+      callback(request.responseText);
+  };
+
+  request.open("GET", url, true);
+  request.send(null);
+}
+
+function getArticleBody(response) {
+  var content = document.createElement("div");
+  content.innerHTML = response;
+  var body = "";
+  nodes = content.querySelectorAll("p, h1, h2, h3, h4, span");
+  nodes.forEach(function (n) {
+    body += n.textContent;
+  });
+  return body;
+}
+
+function call(search) {
+  // query para el buscador de Google
+  var query = search.split(" ").join("+");
+
+  for (var lp = 0; lp < list_pages.length; lp++) {
+    var page = list_pages[lp];
+    var url = "https://www.google.es/search?as_q=" + query + "&as_sitesearch=" + page;
+    console.log(url);
+
+    getHTML(url, function (response) {
+      var res = document.createElement("div");
+      res.innerHTML = response;
+      var data = res.querySelector("#search");
+      var aNodes = data.querySelectorAll("a");
+      var links = [];
+
+      for (var i = 0; i < aNodes.length; i++) {
+        for (var j = 0; j < list_pages.length; j++) {
+          if (
+            aNodes[i].href.startsWith("http") &&
+            aNodes[i].href.includes(list_pages[j]) &&
+            !aNodes[i].href.includes("webcache")
+          ) {
+            links.push(aNodes[i].href);
+          }
+        }
+      }
+
+      console.log(links)
+
+      // Procesamiento del primer link
+      var nlinks = 1;
+      if (links.length > 0) {
+        for (var i = 0; i < nlinks; i++) {
+          getHTML(links[i], function (response) {
+            var body = getArticleBody(response);
+            console.log(body);
+            // Abdul procesa el body
+          });
+        }
+      } else {
+        console.log("No results for article");
+      }
+    });
+  }
 }
--- content.js
+++ content.js
@@ -1,17 +1,18 @@
 // content.js
-function getNewProperties() {
-    var title = document.querySelector('meta[property="og:title"]').getAttribute("content")
-    var description = document.querySelector('meta[property="og:description"]').getAttribute("content")
-    return {
-        title:title,
-        description: description
-    }
+function getArticleProperties() {
+  var title = document
+    .querySelector('meta[property="og:title"]')
+    .getAttribute("content");
+  var description = document
+    .querySelector('meta[property="og:description"]')
+    .getAttribute("content");
+  return {
+    title: title,
+    description: description,
+  };
 }
 
-var properties = getNewProperties()
-
-alert("Title: " + properties.title + " Description: " + properties.description)
-
-var msg = '{"funct":"call", "params":"'+properties.title+'"}';
+var properties = getArticleProperties();
+var msg = {"title": properties.title};
 
 chrome.runtime.sendMessage({ msg: msg });
